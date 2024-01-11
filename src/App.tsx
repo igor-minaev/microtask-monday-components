143e0e7f29ce31c29e6c49b7cc34dcc394114f01
Igor Minayeu <igor.minayeu@gmail.com>
2024-01-11 19:30:13 +0300

    microtask components done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./site/Header";
+import {Body} from "./site/Body";
+import {Footer} from "./site/Footer";
 
 function App() {
     return (
-        <div>Hello world!</div>
-    );
+        <>
+            <Header titleForHeader={'NEW HEADER'}/>
+            <Body titleForBody={'NEW BODY'}/>
+            <Footer titleForFooter={'NEW FOOTER'}/>
+        </>
+    )
+        ;
 }
 
 export default App;
---
+++ src/site/Body.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type BodyType = {
+    titleForBody: string
+}
+
+export const Body = (props:BodyType) => {
+    return <div>{props.titleForBody}</div>
+}
